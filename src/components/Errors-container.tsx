cf8c2ac6e101bfbbc7c487496d9b9af3907b05a3
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2023-08-11 20:41:00 +0300

    Updates

--- src/components/Errors-container.tsx
+++
@@ -1,9 +0,0 @@
-export default function ErrorsContainer({ errorList }: { errorList: string[] }) {
-    console.log('errorList: ', errorList)
-
-    return (
-        <>
-            <div className="error-container">{errorList.length ? errorList.length === 1 ? `Карта ${errorList[0]} не найдена.` : `Карты ${errorList.join(', ')} не найдены.` : ''}</div>
-        </>
-    )
-}
--- src/types.ts
+++
@@ -1,7 +0,0 @@
-export type DeckList = ICard[]
-
-export interface ICard {
-    count: number
-    name: string
-    image: string
-}
